321c43dde495e72b60bb20f558aed5fec82ab385
Dear_Aqua <38954910+DearAqua@users.noreply.github.com>
2018-08-01 21:19:42 +0800

    Add files via upload

---
+++ #005-Var.py
@@ -0,0 +1,34 @@
+name = "Viceroy.Vivi" # 字符串
+rela_degree = 230.5 # 浮点型 
+rela_level =  3 # 赋值整型变量
+print(name)
+print(rela_degree)
+print(rela_level)
+
+print ("==================")
+user_input=""
+a=b=c="无可奉告"
+gf_name,gf_IQ,gf_title = "Aqua","-254","Dear"
+print ("还需要了解什么？")
+print ("1:选项一,2:选项二")
+user_input = int(input())
+if user_input==1:
+    print(a)
+if user_input==2:
+    print(gf_name)
+    print(gf_IQ)
+    print(gf_title)
+
+print ("==================")
+var = 123
+print(var)
+var = True
+print(var)
+var = "You are Silly Boy"
+print(var)
+
+print ("==================")
+value = 2 * 4 + 1
+print(value)
+value = 2 < 5
+print(value)
