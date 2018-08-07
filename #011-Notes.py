4bd25a81e55bdfcbd576c49ce5a6f95b08bbdb86
Dear_Aqua <38954910+DearAqua@users.noreply.github.com>
2018-08-07 19:07:23 +0800

    Add files via upload

---
+++ #011-Notes.py
@@ -0,0 +1,16 @@
+# 单行注释
+print('这是#型- 单行注释')
+
+'''
+blablablabla
+这是多行注释，用三个单引号
+blablablabla
+'''
+print("这是'''型-多行注释")
+      
+"""
+blablablabla
+这是多行注释，用三个双引号
+blablablabla
+"""      
+print('这是"""型-多行注释')
