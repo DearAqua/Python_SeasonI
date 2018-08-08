177bba0a0f684fc4a753023e069764dc20fb0014
Dear_Aqua <38954910+DearAqua@users.noreply.github.com>
2018-08-08 20:29:56 +0800

    Add files via upload

---
+++ #014-Number.py
@@ -0,0 +1,18 @@
+a,b,c,d = 20,5.5,True,4+3j
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+isinstance(a,float)
+isinstance(a,int)
+isinstance(a,bool)
+isinstance(a,complex)
+
+print(5 + 4)  # 加法
+print(4.3 - 2) # 减法
+print(3 * 7)  # 乘法
+print(2 / 4)  # 除法，得到一个浮点数
+print(2 // 4) # 除法，得到一个整数
+print( 17 % 3) # 取余 
+print(2 ** 5) # 乘方
