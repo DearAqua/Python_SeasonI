341a3c61247dca6ebfb5f2266e40558bab110b34
Dear_Aqua <38954910+DearAqua@users.noreply.github.com>
2018-08-09 19:19:30 +0800

    Add files via upload

---
+++ #015-Str.py
@@ -0,0 +1,12 @@
+var = 'Viceroy'
+ 
+print (var)          # 输出字符串
+print (var[0:-1])    # 输出第一个到倒数第二个的所有字符
+print (var[0])       # 输出字符串第一个字符
+print (var[2:5])     # 输出从第三个开始到第五个的字符
+print (var[2:])      # 输出从第三个开始的后的所有字符
+print (var * 2)      # 输出字符串两次
+print (var + "连接") # 连接字符串
+
+print('Vice\roy')
+print(r'Vice\roy')
