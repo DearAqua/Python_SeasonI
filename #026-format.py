9d45f32cb380ecd754a15071bbeb4ca3ce019a4a
Dear_Aqua <38954910+DearAqua@users.noreply.github.com>
2018-08-22 21:17:21 +0800

    Add files via upload

---
+++ #026-format.py
@@ -0,0 +1,18 @@
+#   str 转 int
+print('输入一个数值')
+sum1 = int(input())
+print(sum1)
+#  int 转 str
+ages=17
+print('My age is ' + str(ages))
+print('Your age is ' + str(16))
+
+name = 'Vivi'
+title = 'Viceroy'
+ages = 17
+height = 183.5
+money = 2000.0333333
+print('%s is great %s' % (name,title))
+print('My age is %s' % ages)
+print("I'm %f" % height)
+print('I have %.2f' % money)
