01c7ca72ce0fb70b8497fe7ffe1c2f23241c3223
Dear_Aqua <38954910+DearAqua@users.noreply.github.com>
2018-08-23 19:30:12 +0800

    Add files via upload

---
+++ #028-module.py
@@ -0,0 +1,27 @@
+#   安装、下载模块
+#在os下输入pip install '模块包名'
+# 正则表达式库
+pip install re
+# 词频统计库
+pip install collections 
+# numpy数据处理库
+pip install numpy  
+# 结巴分词
+pip install jieba  
+# 词云展示库
+pip install wordcloud 
+# 图像处理库
+pip install PIL   
+# 图像展示库
+pip install matplotlib.pyplot 
+
+#   导入模块
+#在python下输入import '模块包名'
+import re
+import numpy
+import collections
+
+#   搜索路径
+#在os下输入sys
+sys.path
+sys.path.append('追加的搜索路径')
