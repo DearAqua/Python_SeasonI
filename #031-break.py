a7d84503d33b503bad2e69230bf76b109dcd4fae
Dear_Aqua <38954910+DearAqua@users.noreply.github.com>
2018-08-26 17:45:03 +0800

    Add files via upload

---
+++ #031-break.py
@@ -0,0 +1,12 @@
+for letter in 'Python':
+   if letter == 'h':
+      break
+   print('当前字母 :', letter)
+
+  
+var = 10
+while var > 0:              
+   print('当前变量值 :', var)
+   var = var -1
+   if var == 5:
+      break
