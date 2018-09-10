66b367ef84b3789f8959f56359deb9abb85970e1
Dear_Aqua <38954910+DearAqua@users.noreply.github.com>
2018-09-10 20:40:08 +0800

    Add files via upload

---
+++ pyec#002-sqrt.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  9 16:29:44 2018
+
+@author: lizy52
+"""
+#   user input
+num = float(input('input any number'))
+
+#   deal
+num_sqrt = num ** 0.5
+
+#   EasyCase#002 square-root-calculate
+print('sqrt %0.3f result to %0.3f'%(num ,num_sqrt))
+
+
+import cmath
+ 
+num = int(input("try complex number"))
+
+num_sqrt = cmath.sqrt(num)
+
+print('{0} square root {1:0.3f}+{2:0.3f}j'.format(num ,num_sqrt.real,num_sqrt.imag))
+
+print(2 ** 4)
