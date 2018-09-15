64be47ffbd78b79e0aa97415cfd53d8279e2d6b7
Dear_Aqua <38954910+DearAqua@users.noreply.github.com>
2018-09-15 17:56:04 +0800

    Add files via upload

---
+++ S2_EasyCase/pyec#006-bodh.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 13 09:24:05 2018
+
+@author: lizy52
+"""
+
+
+#   二进制
+def dec2bin(num):
+    l = []
+    if num < 0:
+        return '-' + dec2bin(abs(num))
+    while True:
+        num, remainder = divmod(num, 2)
+        l.append(str(remainder))
+        if num == 0:
+            return ''.join(l[::-1])
+
+
+
+#   八进制
+def dec2oct(num):
+    l = []
+    if num < 0:
+        return '-' + dec2oct(abs(num))
+    while True:
+        num, remainder = divmod(num, 8)
+        l.append(str(remainder))
+        if num == 0:
+            return ''.join(l[::-1])
+
+    
+
+#   十六进制  
+base = [str(x) for x in range(10)] + [ chr(x) for x in range(ord('A'),ord('A')+6)]
+def dec2hex(num):
+    l = []
+    if num < 0:
+        return '-' + dec2hex(abs(num))
+    while True:
+        num,rem = divmod(num, 16)
+        l.append(base[rem])
+        if num == 0:
+            return ''.join(l[::-1])    
+
+
+dec = int(input("input:"))
+ 
+print("Dec:", dec)
+print("Dec to Bin:", bin(dec))
+print("Dec to Oct:", oct(dec))
+print("Dec to Hex:", hex(dec))
+
+print(dec2bin(dec))
+print(dec2oct(dec))
+print(dec2hex(dec))
