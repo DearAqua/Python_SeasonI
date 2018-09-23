958740717275da3dd7f8bc03f8c1041f15363daa
Dear_Aqua <38954910+DearAqua@users.noreply.github.com>
2018-09-23 09:09:22 +0800

    Add files via upload

---
+++ pyec#013-low2sup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 20 14:47:58 2018
+
+@author: LIZY52
+"""
+
+str1 = "Viceroy Vittorio Veneto"
+# 把所有字符中的小写字母转换成大写字母
+print(str1.upper())          
+# 把所有字符中的大写字母转换成小写字母
+print(str1.lower())          
+# 把第一个字母转化为大写字母，其余小写
+print(str1.capitalize())     
+# 把每个单词的第一个字母转化为大写，其余小写
+print(str1.title())          
+
+print('======================================')
+
+trystring = input()
+
+print(trystring.upper())          
+print(trystring.lower())          
+print(trystring.capitalize())     
+print(trystring.title())  
