d0d9eb36ce29fcbe5efa323d8e758df8373f80f8
Dear_Aqua <38954910+DearAqua@users.noreply.github.com>
2018-09-24 18:39:06 +0800

    Add files via upload

---
+++ S2_EasyCase/pyec#014-Chr2AscII.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 21 10:06:57 2018
+
+@author: LIZY52
+"""
+
+#    input chr
+c = input("请输入一个字符: ")
+ 
+#    input ord
+a = int(input("请输入一个ASCII码: "))
+  
+print( c + " 的ASCII 码为", ord(c))
+print( a , " 对应的字符为", chr(a))
