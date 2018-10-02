2fb048ef5818e977180a88146388f3917cb62b3d
Dear_Aqua <38954910+DearAqua@users.noreply.github.com>
2018-10-02 21:48:26 +0800

    Add files via upload

---
+++ S2_EasyCase/pyec#022-Fibonacci.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 29 10:31:55 2018
+
+@author: LIZY52
+"""
+
+#   递归
+def recur_fibo(n):
+   if n <= 1:
+       return n
+   else:
+       return(recur_fibo(n-1) + recur_fibo(n-2))
+  
+nterms = int(input("您要输出几项? "))
+
+if nterms <= 0:
+   print("输入正数")
+else:
+   print("斐波那契数列:")
+   for i in range(nterms):
+       print(recur_fibo(i))
+
+
+#   普通的 while 循环式
+nterms = int(input("你需要几项？"))
+
+n1 = 0
+n2 = 1
+count = 2
+ 
+if nterms <= 0:
+   print("请输入一个正整数。")
+elif nterms == 1:
+   print("斐波那契数列：")
+   print(n1)
+else:
+   print("斐波那契数列：")
+   print(n1,",",n2,end=" , ")
+   while count < nterms:
+       nth = n1 + n2
+       print(nth,end=" , ")
+       n1 = n2
+       n2 = nth
+       count += 1
